55c489428fd3a2fc2553c971843756f192b07dc0
red2fred2 <red2fred2base@live.com>
2023-06-23 02:08:35 -0500

    Get set up to use gradient descent

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
 mod graph;
+mod ml;
 mod math;
 
 fn main() -> Result<(), String> {
-	let num_points = 50;
-
-	let control_points = vec![
-		vec![0.0, 0.0],
-		vec![60.0, 70.0],
-		vec![140.0, 30.0],
-		vec![200.0, 100.0],
-	];
-
 	let domain_min = 0.0;
 	let domain_max = 200.0;
 
-	let points = math::calculate_fn_points(num_points, sin, domain_min, domain_max);
+	let control_points = vec![
+		vec![0.0, 50.0],
+		vec![200.0, 99.46791233116909],
+	];
+
+	// Regress to best spline
+	let samples = 50;
+	let error = ml::fitness::fitness(&control_points, fun, samples);
+
+	println!("Error: {error}");
+
+	// Generate graphs
+	let num_points = 50;
+
+	let points = math::calculate_fn_points(num_points, fun, domain_min, domain_max);
 	let func = graph::points_to_graph_data(&points);
 
 	let points = math::calculate_bezier_points(&control_points, num_points);
 	let spline = graph::points_to_graph_data(&points);
 
 	let control_point_data = graph::points_to_graph_data(&control_points);
-	graph::show_spline_vs_fn(&spline, &control_point_data, &func)
+	graph::show_spline_vs_fn(&spline, &control_point_data, &func)?;
+
+	Ok(())
 }
 
-fn sin(x: f64) -> f64 {
+fn fun(x: f64) -> f64 {
 	50.0 * (x/25.0).sin() + 50.0
 }
---
+++ src/ml/fitness.rs
@@ -0,0 +1,24 @@
+type Point = Vec<f64>;
+
+pub fn fitness<F>(control_points: &Vec<Point>, test_fn: F, samples: u32) -> f32
+where F: Fn(f64) -> f64 {
+	let delta = 1.0 / samples as f64;
+
+	let mut mean_squared_error: f32 = 0.0;
+
+	for i in 0..=samples {
+		// Calculate curve
+		let u = i as f64 * delta;
+		let point = crate::math::calculate_bezier_curve(control_points, u);
+		let y_curve = point[1];
+
+		// Calculate fn
+		let x = point[0];
+		let y_fn = test_fn(x);
+
+		// Calculate error
+		mean_squared_error += ((y_fn - y_curve) * (y_fn - y_curve)) as f32;
+	}
+
+	mean_squared_error.sqrt()
+}
