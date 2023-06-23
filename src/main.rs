d9b200d2c61a79835a9701dcdcd7baa62b735339
red2fred2 <red2fred2base@live.com>
2023-06-23 02:24:18 -0500

    Get start and end points set for regression

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,14 @@ mod ml;
 mod math;
 
 fn main() -> Result<(), String> {
+	// Regress to best spline
 	let domain_min = 0.0;
 	let domain_max = 200.0;
 
-	let control_points = vec![
-		vec![0.0, 50.0],
-		vec![200.0, 99.46791233116909],
-	];
-
-	// Regress to best spline
-	let samples = 50;
-	let error = ml::fitness::fitness(&control_points, fun, samples);
-
-	println!("Error: {error}");
+	let control_points = ml::regress_spline(fun, domain_min, domain_max);
 
 	// Generate graphs
-	let num_points = 50;
+	let num_points = 150;
 
 	let points = math::calculate_fn_points(num_points, fun, domain_min, domain_max);
 	let func = graph::points_to_graph_data(&points);
--- src/ml/mod.rs
+++ src/ml/mod.rs
@@ -1,1 +1,21 @@
 pub mod fitness;
+
+type Point = Vec<f64>;
+
+pub fn regress_spline<F>(func: F, domain_min: f64, domain_max: f64) -> Vec<Point>
+where F: Fn(f64) -> f64 {
+	// Set start and end points
+	let start_point = vec![domain_min, func(domain_min)];
+	let end_point = vec![domain_max, func(domain_max)];
+
+
+	let samples = 15;
+	let error = fitness::fitness(&vec![start_point.clone(), end_point.clone()], func, samples);
+
+	println!("Error: {error}");
+
+	vec![
+		start_point,
+		end_point
+	]
+}
