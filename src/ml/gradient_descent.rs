96b587ec0c1743b0c977cc7f377f217d95e39614
red2fred2 <red2fred2base@live.com>
2023-07-01 15:55:07 -0500

    Move init_data back out to ml mod

--- src/ml/gradient_descent.rs
+++ src/ml/gradient_descent.rs
@@ -3,7 +3,7 @@ type Point = Vec<f64>;
 pub fn gradient_descent<F>(start_point: &Point, end_point: &Point, test_fn: &F, num_controls: u8)
 -> Vec<f64>
 where F: Fn(f64) -> f64 {
-	let mut data = init_data(start_point, end_point, num_controls);
+	let mut data = super::init_data(start_point, end_point, num_controls);
 
 	let samples = 100;
 
@@ -47,24 +47,4 @@ where F: Fn(f64) -> f64 {
 
 	println!("Best fitness: {best_fitness}");
 	best_data
-}
-
-fn init_data(start_point: &Point, end_point: &Point, num_controls: u8) -> Vec<f64> {
-	let x_0 = start_point[0];
-	let y_0 = start_point[1];
-	let x_1 = end_point[0];
-	let y_1 = end_point[1];
-
-	let delta = (x_1 - x_0) / (num_controls + 1) as f64;
-	let mut data = Vec::new();
-
-	for i in 1..num_controls {
-		let x = x_0 + i as f64 * delta;
-		data.push(x);
-
-		let y = crate::math::linear_interpolate(x_0, y_0, x_1, y_1, x);
-		data.push(y)
-	}
-
-	data
 }
--- src/ml/mod.rs
+++ src/ml/mod.rs
@@ -26,3 +26,23 @@ pub fn build_control_points(data: &Vec<f64>, start_point: &Point, end_point: &Po
 
 	control_points
 }
+
+fn init_data(start_point: &Point, end_point: &Point, num_controls: u8) -> Vec<f64> {
+	let x_0 = start_point[0];
+	let y_0 = start_point[1];
+	let x_1 = end_point[0];
+	let y_1 = end_point[1];
+
+	let delta = (x_1 - x_0) / (num_controls + 1) as f64;
+	let mut data = Vec::new();
+
+	for i in 1..num_controls {
+		let x = x_0 + i as f64 * delta;
+		data.push(x);
+
+		let y = crate::math::linear_interpolate(x_0, y_0, x_1, y_1, x);
+		data.push(y)
+	}
+
+	data
+}
