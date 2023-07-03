c5a71e059bdc7f3fe3b5beffc2b5d4c6a303775f
red2fred2 <red2fred2base@live.com>
2023-07-03 03:08:01 -0500

    Add basis function calculations for B-splines

--- src/spline/b_spline.rs
+++ src/spline/b_spline.rs
@@ -32,3 +32,41 @@ fn find_span(degree: u8, knots: Vec<f32>, u: f32) -> usize {
 
 	mid
 }
+
+/// Find the values of each basis function at u
+///
+/// This algorithm is ripped right from the NURBS book, it may need to be
+/// modified to work for tensor product surfaces.
+///
+/// * `degree`	- The polynomial degree of the basis functions
+/// * `knots`	- A vector containing the u value of each knot
+/// * `span`	- Which span the u value is in
+/// * 'u'		- The u value
+fn basis_functions(degree: u8, knots: Vec<f32>, span: usize, u: f32) -> Vec<f32> {
+	let mut outputs = Vec::new();
+	outputs.push(1.0);
+	let mut left = Vec::new();
+	left.push(0.0);
+	let mut right = Vec::new();
+	right.push(0.0);
+
+	for i in 1..=degree as usize {
+		let left_side = u - knots[span + 1 - i];
+		left.push(left_side);
+
+		let right_side = knots[span + i] - u;
+		right.push(right_side);
+
+		let mut saved = 0.0;
+
+		for j in 0..i {
+			let temp = outputs[j] / (right[j + 1] + left[i - j]);
+			outputs[j] = saved + right[j + 1] * temp;
+			saved = left[i - j] * temp;
+		}
+
+		outputs[i] = saved;
+	}
+
+	outputs
+}
